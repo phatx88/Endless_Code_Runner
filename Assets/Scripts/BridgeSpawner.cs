2aeb7a6f41bfce117b02f0b728af85cab9591a07
Phat Luong <phat.x.luong@gmail.com>
2024-05-22 12:37:11 +0700

    Add Animation and Bridge Spawner 5-22-2024

---
+++ Assets/Scripts/BridgeSpawner.cs
@@ -0,0 +1,234 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeSpawner : MonoBehaviour
+{
+    // Array of prefabs to be used for the bridge segments
+    public GameObject[] bridgePrefabs;
+
+    // Enum to define different types of bridge segments
+    enum enType
+    {
+        L_Corner,
+        Straight,
+        R_Corner,
+    }
+
+    // Enum to define possible directions for the segments
+    enum enDirection
+    {
+        North,
+        East,
+        West,
+    }
+
+    // Class to hold information about each bridge segment
+    class Segments
+    {
+        public GameObject segPrefab;
+        public enType segType;
+
+        public Segments(GameObject segPrefab, enType segType)
+        {
+            this.segPrefab = segPrefab;
+            this.segType = segType;
+        }
+    };
+
+    // List to keep track of active segments in the scene
+    List<GameObject> activeSegments = new List<GameObject>();
+
+    // Current segment being created
+    Segments segment;
+    
+    // Starting coordinate for spawning segments
+    Vector3 spawnCoord = new Vector3(0, 0, -6.0f);
+    
+    // Current and next direction for segment placement
+    enDirection segCurrentdirection = enDirection.North;
+    enDirection segNextDirection = enDirection.North;
+    
+    // Reference to the player's transform
+    Transform playerTransform;
+
+    float segLenght = 6.0f;
+    float segWidth = 3.0f;
+    int segsOnScreen = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize the first segment
+        segment = new Segments(bridgePrefabs[0], enType.Straight);
+
+        // Find the player's transform in the scene
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        
+        // Initialize the segments
+        InitializeSegments();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if player has triggered a segment change
+        PlayerTrigger();
+    }
+
+    // Method to initialize segments at the start
+    void InitializeSegments()
+    {
+        for (int i = 0; i < segsOnScreen; i++)
+        {
+            SpawnSegments();
+        }
+    }
+
+    // Method to determine the type and prefab of the next segment
+    void CreateSegments()
+    {
+        switch (segCurrentdirection)
+        {
+            case enDirection.North:
+                segment.segType = (enType)Random.Range(0, 3);
+                if (segment.segType == enType.Straight)
+                {
+                    segment.segPrefab = bridgePrefabs[Random.Range(0, 6)];
+                }
+                else if (segment.segType == enType.L_Corner)
+                {
+                    segment.segPrefab = bridgePrefabs[7];
+                }
+                else if (segment.segType == enType.R_Corner)
+                {
+                    segment.segPrefab = bridgePrefabs[6];
+                }
+                break;
+            case enDirection.East:
+                segment.segType = (enType)Random.Range(0, 2);
+                if (segment.segType == enType.Straight)
+                {
+                    segment.segPrefab = bridgePrefabs[Random.Range(0, 6)];
+                }
+                else if (segment.segType == enType.L_Corner)
+                {
+                    segment.segPrefab = bridgePrefabs[7];
+                }
+                break;
+            case enDirection.West:
+                segment.segType = (enType)Random.Range(1, 3);
+                if (segment.segType == enType.Straight)
+                {
+                    segment.segPrefab = bridgePrefabs[Random.Range(0, 6)];
+                }
+                else if (segment.segType == enType.R_Corner)
+                {
+                    segment.segPrefab = bridgePrefabs[6];
+                }
+                break;
+        }
+    }
+
+    // Method to spawn a new segment in the scene
+    void SpawnSegments()
+    {
+        GameObject prefabToInstatiate = segment.segPrefab;
+        Quaternion prefabRotation = Quaternion.identity;
+        Vector3 offSet = new Vector3(0, 0, 0);
+
+        switch (segCurrentdirection)
+        {
+            case enDirection.North:
+                if (segment.segType == enType.Straight)
+                {
+                    prefabRotation = Quaternion.Euler(0, 0, 0);
+                    segNextDirection = enDirection.North;
+                    spawnCoord.z += segLenght;
+                }
+                else if (segment.segType == enType.R_Corner)
+                {
+                    prefabRotation = Quaternion.Euler(0, 0, 0);
+                    segNextDirection = enDirection.East;
+                    spawnCoord.z += segLenght;
+                    offSet.z += segLenght + segWidth / 2;
+                    offSet.x += segWidth / 2;
+                }
+                else if (segment.segType == enType.L_Corner)
+                {
+                    prefabRotation = Quaternion.Euler(0, 0, 0);
+                    segNextDirection = enDirection.West;
+                    spawnCoord.z += segLenght;
+                    offSet.z += segLenght + segWidth / 2;
+                    offSet.x -= segWidth / 2;
+                }
+                break;
+
+            case enDirection.East:
+                if (segment.segType == enType.Straight)
+                {
+                    prefabRotation = Quaternion.Euler(0, 90, 0);
+                    segNextDirection = enDirection.East;
+                    spawnCoord.x += segLenght;
+                }
+                else if (segment.segType == enType.L_Corner)
+                {
+                    prefabRotation = Quaternion.Euler(0, 90, 0);
+                    segNextDirection = enDirection.North;
+                    spawnCoord.x += segLenght;
+                    offSet.z += segWidth / 2;
+                    offSet.x += segLenght + segWidth / 2;
+                }
+                break;
+
+            case enDirection.West:
+                if (segment.segType == enType.Straight)
+                {
+                    prefabRotation = Quaternion.Euler(0, -90, 0);
+                    segNextDirection = enDirection.West;
+                    spawnCoord.x -= segLenght;
+                }
+                else if (segment.segType == enType.R_Corner)
+                {
+                    prefabRotation = Quaternion.Euler(0, -90, 0);
+                    segNextDirection = enDirection.North;
+                    spawnCoord.x -= segLenght;
+                    offSet.z += segWidth / 2;
+                    offSet.x -= segLenght + segWidth / 2;
+                }
+                break;
+        }
+
+        if (prefabToInstatiate != null)
+        {
+            GameObject spawnedPrefab = Instantiate(prefabToInstatiate, spawnCoord, prefabRotation);
+            activeSegments.Add(spawnedPrefab);
+            spawnedPrefab.transform.parent = this.transform;
+        }
+
+        segCurrentdirection = segNextDirection;
+        spawnCoord += offSet;
+    }
+
+    // Method to remove the oldest segment from the scene
+    void RemoveSegments()
+    {
+        Destroy(activeSegments[0]);
+        activeSegments.RemoveAt(0);
+    }
+
+    // Method to handle the player triggering segment changes
+    void PlayerTrigger()
+    {
+        GameObject go = activeSegments[0];
+
+        // If the player is far enough from the first segment, create and spawn new segments, then remove the old one
+        if (Mathf.Abs(Vector3.Distance(playerTransform.position, go.transform.position)) > 16.0f)
+        {
+            CreateSegments();
+            SpawnSegments();
+            RemoveSegments();
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,71 +4,79 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    enum enDirection { North, East, West}; //a numerator that can enumerate player direction
-    
-    CharacterController characterController;
-    Vector3 playerVector; //Player direction 
-    enDirection playerDirection = enDirection.North; //set default direction (north = 0)
-    enDirection playerNextDirection = enDirection.North;
+    // Enumeration for player direction
+    enum enDirection { North, East, West };
 
+    // Components and player properties
+    CharacterController characterController;
+    Vector3 playerVector; // Player movement vector
+    enDirection playerDirection = enDirection.North; // Default direction (North)
+    enDirection playerNextDirection = enDirection.North; // Next direction for the player
+    Animator anim;
+
+    // Player speed and movement-related variables
     public float playerStartSpeed = 10.0f;
     float playerSpeed;
-    float gValue = 10.0f; //set gravity value
-    float translationFactor = 10.0f; // Smoothen the turning of direction
+    float gValue = 20.0f; // Gravity value
+    float translationFactor = 10.0f; // Smoothens the turning of direction
+    float jumpForce = 1.5f;
+
+    // Additional variable for vertical velocity
+    float verticalVelocity = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerSpeed = playerStartSpeed;
-        characterController = GetComponent<CharacterController>();
-        playerVector = new Vector3(0 , 0, 1) * playerSpeed * Time.deltaTime;
+        playerSpeed = playerStartSpeed; // Initialize player speed
+        characterController = GetComponent<CharacterController>(); // Get CharacterController component
+        anim = GetComponent<Animator>(); // Get Animator component
+        playerVector = new Vector3(0, 0, 1) * playerSpeed * Time.deltaTime; // Initialize player movement vector
     }
 
     // Update is called once per frame
-    void Update()   
+    void Update()
     {
-        PlayerLogic();
+        PlayerLogic(); // Call the logic function each frame
     }
 
     void PlayerLogic()
     {
-        playerSpeed += 0.005f * Time.deltaTime; // increasing playerspeed as player traverse through the game
+        playerSpeed += 0.005f * Time.deltaTime; // Gradually increase player speed over time
 
         #region Horizontal Direction 
-        //Set Logic to Horizontal direction the player when press Key 'G'  or 'F' on keyboard, left and right direction
-        //Making sure that Player will not turning more than 180 degree toward the South diraction (make sure only move N, E and W)
-        //For Example if press G first time will turn right, press again will turn North.
-        if (Input.GetKeyDown(KeyCode.G)) //check first condition if press G key
+        // Handles player direction change based on key inputs 'G' and 'F'
+        if (Input.GetKeyDown(KeyCode.E)) // When 'G' key is pressed
         {
             switch (playerDirection)
             {
                 case enDirection.North:
                     playerNextDirection = enDirection.East;
-                    transform.rotation = Quaternion.Euler(0, 90, 0);  //If player facing North and turning East, player move 90 degree right
+                    transform.rotation = Quaternion.Euler(0, 90, 0);  // Rotate player to face East
                     break;
                 case enDirection.West:
                     playerNextDirection = enDirection.North;
-                    transform.rotation = Quaternion.Euler(0, 0, 0);  //If player facing West and turning North, player move straight
+                    transform.rotation = Quaternion.Euler(0, 0, 0);  // Rotate player to face North
                     break;
             }
         }
-        else if (Input.GetKeyDown(KeyCode.F)) //check second condition if press F key
+        else if (Input.GetKeyDown(KeyCode.Q)) // When 'F' key is pressed
         {
             switch (playerDirection)
             {
                 case enDirection.North:
                     playerNextDirection = enDirection.West;
-                    transform.rotation = Quaternion.Euler(0, -90, 0);  
+                    transform.rotation = Quaternion.Euler(0, -90, 0);  // Rotate player to face West
                     break;
                 case enDirection.East:
                     playerNextDirection = enDirection.North;
-                    transform.rotation = Quaternion.Euler(0, 0, 0); 
+                    transform.rotation = Quaternion.Euler(0, 0, 0);  // Rotate player to face North
                     break;
             }
         }
 
-        playerDirection = playerNextDirection;
+        playerDirection = playerNextDirection; // Update player direction
 
+        // Update player movement vector based on direction
         if (playerDirection == enDirection.North)
         {
             playerVector = Vector3.forward * playerSpeed * Time.deltaTime;
@@ -83,7 +91,7 @@ public class PlayerController : MonoBehaviour
         }
         #endregion
 
-        //Set Logic Horizontal movement of the player
+        // Set horizontal movement based on direction and input
         switch (playerDirection)
         {
             case enDirection.North:
@@ -95,15 +103,79 @@ public class PlayerController : MonoBehaviour
             case enDirection.West:
                 playerVector.z = Input.GetAxisRaw("Horizontal") * translationFactor * Time.deltaTime;
                 break;
-
         }
-        //Set Logic for Vertical Movement of the Player
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            DoSliding(); // Trigger sliding when the down arrow key is pressed
+        }
+        #region vertical movement logic 
+        //// Vertical movement logic
+        //if (characterController.isGrounded)
+        //{
+        //    playerVector.y = -0.2f; // Ensure player stays grounded
+        //}
+        //else
+        //{
+        //    playerVector.y -= gValue * Time.deltaTime; // Apply gravity if not grounded
+        //}
+
+        //// Jumping logic
+        //if (Input.GetKeyDown(KeyCode.Space) && characterController.isGrounded)
+        //{
+        //    anim.SetTrigger("isJumping"); // Trigger jump animation
+        //    playerVector.y = Mathf.Sqrt(jumpForce * gValue); // Calculate jump force
+        //}
+        #endregion
+
+        #region Adding Vertical Velocity to vertical movement logic 
+        
+
+        // Vertical movement logic
         if (characterController.isGrounded)
         {
-            playerVector.y = -0.2f; //push against the ground
-        }
-        else playerVector.y = playerVector.y - (gValue * Time.deltaTime); //if not touch ground fall to gravity
-        characterController.Move(playerVector);
+            if (verticalVelocity < 0)
+            {
+                verticalVelocity = -0.2f; // Ensure player stays grounded
+            }
 
+            // Jumping logic
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.SetTrigger("isJumping"); // Trigger jump animation
+                verticalVelocity = Mathf.Sqrt(jumpForce * gValue); // Calculate jump force
+            }
+        }
+        else
+        {
+            verticalVelocity -= gValue * Time.deltaTime; // Apply gravity if not grounded
+        }
+
+        // Apply vertical velocity to player vector
+        playerVector.y = verticalVelocity * Time.deltaTime;
+        #endregion
+
+
+        characterController.Move(playerVector); // Move the character controller
+    }
+
+    void DoSliding()
+    {
+        // Adjust CharacterController for sliding
+        characterController.height = 1f;
+        characterController.center = new Vector3(0, 0.5f, 0);
+        characterController.radius = 0;
+        StartCoroutine(ReEnableCC()); // Reset CharacterController after sliding
+        anim.SetTrigger("isSliding"); // Trigger sliding animation
+    }
+
+    IEnumerator ReEnableCC()
+    {
+        yield return new WaitForSeconds(0.5f); // Wait for 0.5 seconds
+
+        // Reset CharacterController dimensions
+        characterController.height = 2f;
+        characterController.center = new Vector3(0, 1f, 0);
+        characterController.radius = 0.2f;
     }
 }
